88c8ae41434d8c5a900963eb958d5dce7ef28bdc
Med Khalil Ismail <medkhalil.ismail@esprit.tn>
2023-11-08 06:13:41 +0100

    Rendu Prosit 2

---
+++ src/Zoo.java
@@ -0,0 +1,66 @@
+public class Zoo {
+    Animal[] animals;
+    String name,city;
+    int nbrCages;
+    int nbrAnimals=0;
+
+
+    public Zoo(String name, String city, int nbrCages) {
+        animals = new Animal[nbrCages];
+        this.name = name;
+        this.city = city;
+        this.nbrCages = nbrCages;
+    }
+
+    /*void diplayZoo(){
+        System.out.println("Name: " + name + ", City: " + city + ", N° Cages/Animals: " + nbrCages);
+    }*/
+
+    @Override
+    public String toString(){
+        return "Name: " + name + ", City: " + city + ", N° Cages/Animals: " + nbrCages;
+    }
+
+    boolean addAnimal(Animal animal){
+        if ((nbrCages>nbrAnimals)&&(searchAnimal(animal)==-1)) {
+            animals[nbrAnimals] = animal;
+            nbrAnimals++;
+            return true;
+        }
+        else return false;
+    }
+
+    void displayAnimals(){
+        System.out.println("La liste des animaux de " + name + ":");
+        for(int i=0;i<nbrAnimals;i++){
+            System.out.println(animals[i]);
+        }
+    }
+
+    int searchAnimal(Animal animal){
+        int index = -1;
+        for(int i=0;i<nbrAnimals;i++){
+            if(animals[i].name == animal.name)
+                index = i;
+        }
+        return index;
+    }
+
+    boolean removeAnimal(Animal animal){
+        int indexAnimal = searchAnimal(animal);
+        if (indexAnimal == -1){
+            System.out.println(animal.name + "n'est pas dans la liste des animaux");
+            return false;
+        }
+        else {
+            for(int i=indexAnimal;i<nbrAnimals;i++){
+                animals[i] = animals[i+1];
+                animals[i+1] = null;
+            }
+            this.nbrAnimals--;
+            System.out.println(animal.name + " a été supprimé avec succés");
+            return true;
+        }
+    }
+}
+
--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,9 +1,14 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ZooManagement {
     int nbrCages = 20;
     String zooName = "my zoo";
     public static void main(String[] args) {
+
+        /* PROSIT 1
+        int nbrCages = 20;
+        String zooName = "my zoo";
         ZooManagement zm = new ZooManagement();
         System.out.println(zm.zooName + " comporte " + zm.nbrCages + " cage(s)");
 
@@ -14,5 +19,21 @@ public class ZooManagement {
         System.out.println("Entrer le nom de votre zoo:");
         zm1.zooName=sc.next();
         System.out.println(zm1.zooName + " comporte " + zm1.nbrCages + " cage(s)");
+        */
+        /* PROSIT 2 */
+        Animal lion = new Animal("Lions","King",5,true);
+        Animal singe = new Animal("Singes","Banana",3,true);
+        Animal singe1 = new Animal("Singes","Banana",3,true);
+        Zoo myZoo = new Zoo("Nabeul","Cap Zoo",25);
+        //myZoo.diplayZoo();
+        System.out.println(lion.toString());
+        System.out.println(myZoo.toString());
+        System.out.println(myZoo.addAnimal(lion));
+        System.out.println(myZoo.addAnimal(singe));
+        System.out.println(myZoo.addAnimal(singe1));
+        myZoo.displayAnimals();
+        System.out.println(myZoo.searchAnimal(singe));
+        System.out.println(myZoo.removeAnimal(singe));
+
     }
 }
